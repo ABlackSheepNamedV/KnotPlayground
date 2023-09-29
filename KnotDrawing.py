ffc691643869a16363b69ba45ff8e57243ee1be6
HoytHoyt <campmister@gmail.com>
2023-09-28 22:15:05 -0600

    Provided some documentation to the KnotDrawing.py file.

--- KnotDrawing.py
+++ KnotDrawing.py
@@ -4,12 +4,42 @@ import sympy
 import numpy as np
 
 class KnotDrawing:
+    """ A class for reading in images of knots and determining their
+        corresponding Alexander polynomial coefficients. 
+
+        The Alexander polynomial is an invariant of knots that helps identify
+        between similar but distinct knots. The polynomial is calculated by
+        setting up an (n,n+2) matrix with the rows corresponding to the n
+        crossings and the n+2 regions separated by the knot. We then trace along
+        the knot, where at each undercrossing we populate the matrix with:
+            +t to the region after the crossing to the left
+            -1 to the region after the crossing to the right
+            -t to the region before the crossing to the left
+            +1 to the region before the crossing to the right.
+            +0 to regions not adjacent to the crossing. 
+
+        For more detailed information, see the Wikipedia page:
+        https://en.wikipedia.org/wiki/Alexander_polynomial
+    """
     
     def __init__(self, filename):
+        """ Initialize the KnotDrawing object from a file. 
+
+            We assume that the knot consists of a png file with white, black,
+            and gray pixels. White and gray pixels outline the knot, with gray
+            pixels indicating that a portion of the knot is next to an
+            undercrossing. 
+        """
         knotImage = cv2.imread(filename)
         self.knotData = knotImage[:,:,0]
-        
+
+
+
+    ### Knot tracing methods:
+
     def initPath(self):
+        """ Initialize a path used to help trace along the boundary of a knot. 
+        """
         knotPath = np.where(self.knotData==255)
         
         self.start = np.array([knotPath[0][0], knotPath[1][0]])
@@ -19,20 +49,10 @@ class KnotDrawing:
         
         return
 
-    def initMatrix(self):
-        # Identify regions
-        self.regions = ndimage.label(self.knotData==0)[0] - 1
-        self.numRegions = np.max(self.regions) + 1
-        self.numCrossings = self.numRegions-2
-        
-        # Initialize records for the Alexander matrix
-        self.t = sympy.symbols("t")
-        self.alexMat = sympy.zeros(self.numCrossings, self.numRegions)
-        self.crossingCount = 0
-        
-        return
-    
     def givePos(self, direction="none"):
+        """ Provides the indices for the pixel in the image one step towards
+            the specified direction.
+        """
         
         position = np.copy(self.pos)
         forward = self.step
@@ -47,11 +67,20 @@ class KnotDrawing:
         
         
     def atUndercrossing(self):
+        """ Provides a boolean specifying if the tracing has arrived at an
+            undercrossing. 
+        """
         checkF = self.knotData[self.givePos("front")]
         checkB = self.knotData[self.givePos("back" )]
-        return (checkF==116 and checkB==116)
+
+        undercrossingF = (checkF!=0 and checkF!=255)
+        undercrossingB = (checkB!=0 and checkB!=255)
+
+        return (undercrossingF and undercrossingB)
         
     def updateStepDirection(self):
+        """ Updates the forward facing direction for the current knot tracing.
+        """
         
         checkF = self.knotData[self.givePos("front")]
         checkL = self.knotData[self.givePos("left" )]
@@ -63,8 +92,45 @@ class KnotDrawing:
             self.step = self.step @ -self.turn
         
         return
-    
-    def updateAlexMatrix(self):
+
+
+
+    ### Alexander matrix methods:
+
+    def initMatrix(self):
+        """ Initialize the matrix used to calculate the Alexander polynomial.
+
+            The polynomial utilizes the determinant of a (n,n+2) matrix that
+            corresponds to the n crossings as rows and the n+2 regions separated
+            by the knot as columns.
+
+            See https://en.wikipedia.org/wiki/Alexander_polynomial for more
+            details.
+        """
+
+        # Identify regions
+        self.regions = ndimage.label(self.knotData==0)[0] - 1
+        self.numRegions = np.max(self.regions) + 1
+        self.numCrossings = self.numRegions-2
+        
+        # Initialize records for the Alexander matrix
+        self.t = sympy.symbols("t")
+        self.alexMat = sympy.zeros(self.numCrossings, self.numRegions)
+        self.crossingCount = 0
+        
+        return
+
+    def updateMatrix(self):
+        """ Based on the current position and facing of the knot tracing,
+            update the matrix used to calculate the Alexander polynomial.
+
+            At undercrossings, we provide a -t, +1, +t, -t to the regions
+            immediately to the backleft, backright, frontleft, and frontright
+            respectively. 
+
+            See https://en.wikipedia.org/wiki/Alexander_polynomial for more
+            details.
+        """
         
         regionBL = self.regions[ self.givePos("back-left"  ) ]
         self.alexMat[self.crossingCount, regionBL] = -self.t
@@ -83,6 +149,10 @@ class KnotDrawing:
         return
         
     def createAlexMatrix(self):
+        """ Create the matrix used to calculate the Alexander polynomial knot
+            invariant. See https://en.wikipedia.org/wiki/Alexander_polynomial
+            for more details.
+        """
 
         self.initPath()
         self.initMatrix()
@@ -90,17 +160,19 @@ class KnotDrawing:
         for stepIndex in range(5000):
 
             if self.atUndercrossing():
-                self.updateAlexMatrix()
+                self.updateMatrix()
 
             self.updateStepDirection()
             self.pos += self.step
-
             
             distanceToStart = np.linalg.norm(self.pos - self.start)
             if distanceToStart < 0.1:
                 break
                 
     def alexPoly(self):
+        """ Calculate the Alexander polynomial for the knot of interest.
+        """
+        self.createAlexMatrix()
         alexanderDet = self.alexMat[:,2:].det()
         alexanderPolynomial = sympy.Poly(alexanderDet,self.t)
         coeffs = alexanderPolynomial.coeffs()
@@ -114,5 +186,4 @@ class KnotDrawing:
 
 index = 1
 knot = KnotDrawing(f"images/9_{index}.png")
-knot.createAlexMatrix()
 print(knot.alexPoly())
